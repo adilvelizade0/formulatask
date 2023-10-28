89ce7023531a0bab99b264da9902f642df7465e5
Adil Valizada <adilvalizada@gmail.com>
2023-10-28 12:09:50 +0200

    Some error is fixed

--- src/components/FormulaInput/FormulaInput.component.tsx
+++ src/components/FormulaInput/FormulaInput.component.tsx
@@ -1,12 +1,12 @@
-import { FC, JSX, useState, useRef, useCallback } from "react";
+import { FC, JSX } from "react";
 import FormulaInputStyles from "./FormulaInput.styles.ts";
-import { AutoComplete } from "primereact/autocomplete";
+// import { AutoComplete } from "primereact/autocomplete";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
-
-type Tag = {
-  id: number;
-  name: string;
-};
+//
+// type Tag = {
+//   id: number;
+//   name: string;
+// };
 
 const FormulaInput: FC = (): JSX.Element => {
   return <FormulaInputStyles></FormulaInputStyles>;
